a6dc9ac4a54a0b46fab219985119f2d6117ac6ec
KazukiDaido <k10de302222@gmail.com>
2023-01-16 18:09:04 +0900

    Initial Commit

---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by 大道一輝 on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
